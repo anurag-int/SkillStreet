5a0c637013f399d8cdff35ec39e1ca8bb202b38c
Anurag Gupta <ag6526803@gmail.com>
2024-01-02 20:31:53 +0530

    note Function Updated

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -86,7 +86,7 @@ exports.login = async (req, res) => {
     // Generate JWT token and Compare Password
     if (await bcrypt.compare(password, user.password)) {
       const token = jwt.sign(
-        { email: user.email, id: user._id},
+        { email: user.email, id: user._id },
         process.env.JWT_SECRET,
         {
           expiresIn: "24h",
--- server/controllers/Note.js
+++ server/controllers/Note.js
@@ -1,2 +1,53 @@
-const express = require("express");
 const Note = require("../models/Note");
+
+exports.createNote = async(req, res) => {
+    try{
+        
+    }
+    catch(error){
+        console.log(error.message);
+        return res.status(500).json({
+            status : false,
+            message : "Internal Server Error"
+        })
+    }
+}
+
+exports.deleteNote = async(req, res) => {
+    try{
+
+    }
+    catch(error){
+        console.log(error.message);
+        return res.status(500).json({
+            status : false,
+            message : "Internal Server Error"
+        })
+    }
+}
+
+exports.updateNote = async(req, res) => {
+    try{
+
+    }
+    catch(error){
+        console.log(error.message);
+        return res.status(500).json({
+            message : false,
+            message : "Internal Server Error"
+        })
+    }
+}
+
+exports.getNotes = async(req, res) => {
+    try{
+
+    }
+    catch(error){
+        console.log(error.message);
+        return res.status(500).json({
+            message : false,
+            message : "Internal Server Error"
+        })
+    }
+}
