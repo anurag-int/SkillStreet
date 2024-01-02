ba6749e7837672a833283f5153a534586736df9c
Anurag Gupta <ag6526803@gmail.com>
2024-01-02 20:12:06 +0530

    Schema, routes, authentication, API's added for Users

---
+++ server/middlewares/auth.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/User");
+
+
+exports.auth = async(req, res, next) => {
+    try{
+        
+        const token = req.cookies.token || req.body.token || req.header("Authorization").replace("Bearer", "");
+
+        if(!token){
+            return res.status(401).json({
+                success : false,
+                message : `Token is missing`
+            });
+        }
+
+        try{
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decode);
+            req.user = decode;
+        }   
+        catch(error){
+            return res.status(401).json({
+                success : false,
+                message : `Token is invalid`
+            });
+        }
+        next();
+    }
+    catch(error){
+        return res.status(401).json({
+            success : false,
+            message : `Something went wrong while valadating the token`
+        });
+    }
+} 
